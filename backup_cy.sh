65fc23c3bcd0d9ca74530fabf11680d9d36c03bf
Casey Brant <casey.brant@gmail.com>
2011-09-10 14:27:38 -0500

    I dunno a whole bunch of changes. Whatever.

--- backup_cy.sh
+++ backup_cy.sh
@@ -3,5 +3,5 @@
 day=`date +%Y-%m-%d`
 filename="cy backup $day.tar.gz"
 cd /Users/cjb
-tar czf "$filename" cy.sparsebundle
-mv "$filename" "/Users/cjb/Dropbox/cy backups"
+tar czf "$filename" tc-cy
+mv "$filename" "/Users/cjb/Dropbox/zzz backups"
