0d09801fbf3cf1aa93f2defb8a9c4b6b083cfcfa
Casey Brant <casey.brant@gmail.com>
2014-08-09 17:13:44 -0700

    Don't limit find command to just files in install.sh. fixes #5

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for source_file in $(find . -name "dot_*" -type f -maxdepth 1)
+for source_file in $(find . -name "dot_*" -maxdepth 1)
 do
     target_file=${source_file/.\/dot_/.}
     ln -fs `pwd`/$source_file ~/$target_file
