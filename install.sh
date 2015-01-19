449aac990dc93171681013b4ec514373fa98e486
Casey Brant <casey.brant@gmail.com>
2015-01-19 13:08:12 -0800

    fix paths generated by install script

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ set -e
 
 # for every file in ~/bin that starts with "dot_",
 # make a symlinked dotfile in ~ that maps to that file in ~/bin
-for source_file in $(find . -name "dot_*" -maxdepth 1)
+for file in $(find . -name "dot_*" -maxdepth 1)
 do
-    target_file=${source_file/.\/dot_/.}
-    ln -fs `pwd`/$source_file ~/$target_file
+    source_file=${file##./}                    # strip leading './' from filename
+    target_file=${source_file/.\/dot_/.}       # make .foo version of dot_foo filename
+    ln -fs `pwd`/$source_file ~/$target_file   # symlink ~/.foo files to bin/dot_foo files
 done
 
 touch local_bash_config
 touch ~/.local_vimrc
 rm -rf ~/.vim_backup
 mkdir ~/.vim_backup
-
