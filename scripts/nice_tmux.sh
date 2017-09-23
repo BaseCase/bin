4e007fc142f4880b4f1bfb9b984339fadb29c584
Casey Brant <casey@caseybrant.com>
2017-09-22 19:30:21 -0500

    A nice little interface over attaching/launching tmux sessions

---
+++ scripts/nice_tmux.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+new_session_sentinel="Start New Session"
+
+main() {
+  if [ `count_open_tmux_sessions` -gt "0" ] ; then
+    choice=`present_session_list`
+    attach_to_chosen_session "$choice"
+  else
+    tmux
+  fi
+}
+
+count_open_tmux_sessions() {
+  tmux ls 2>/dev/null | wc -l | awk '{print $1}'
+}
+
+present_session_list() {
+  printf "$new_session_sentinel\n$(tmux ls)" | fzf --height 40%
+}
+
+attach_to_chosen_session() {
+  choice="$1"
+
+  if [ "$choice" == "$new_session_sentinel" ] ; then
+    tmux
+  else
+    tmux attach -t `parse_session_name "$choice"`
+  fi
+}
+
+parse_session_name() {
+  echo "$1" | cut -d ':' -f 1
+}
+main
