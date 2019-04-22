9a08f7fdd4887b3aba488df96cc3bfe27413283e
Casey Brant <casey@caseybrant.com>
2019-04-21 20:07:07 -0500

    Setting up LSP and a little Ruby extra stuff

--- emacs/cjb-computer-programming.el
+++ emacs/cjb-computer-programming.el
@@ -5,6 +5,15 @@
 
 (projectile-global-mode t)
 (global-company-mode t)
+(push 'company-lsp company-backends)
 (setq company-idle-delay 10)
+
+(setq lsp-prefer-flymake :none)
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+
+(add-hook 'ruby-mode-hook #'lsp)
+(add-hook 'enh-ruby-mode-hook #'lsp)
+
+(add-hook 'xref--xref-buffer-mode-hook 'evil-emacs-state)
 
 (provide 'cjb-computer-programming)
