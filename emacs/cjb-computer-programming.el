687fcde404873dc1c22d0e2d016d6662dc788b7b
Casey Brant <casey@caseybrant.com>
2019-04-23 15:37:43 -0500

    Swift LSP setup in Emacs

--- emacs/cjb-computer-programming.el
+++ emacs/cjb-computer-programming.el
@@ -9,8 +9,13 @@
 (setq company-idle-delay 10)
 (setq lsp-prefer-flymake :none)
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+
+(require 'lsp-sourcekit)
+(setq lsp-sourcekit-executable (expand-file-name "~/bin/bin/sourcekit-lsp"))
 (add-hook 'ruby-mode-hook #'lsp)
 (add-hook 'enh-ruby-mode-hook #'lsp)
+(add-hook 'swift-mode-hook #'lsp)
+
 (add-hook 'xref--xref-buffer-mode-hook 'evil-emacs-state)
 (add-hook 'comint-mode-hook
           (lambda ()
