c26523f99518b6e1efe3977d7005ec2d9e4c9525
Casey Brant <casey@caseybrant.com>
2020-01-23 21:05:16 -0600

    a couple of visual enhancements to org-mode

--- emacs/cjb-org-mode-config.el
+++ emacs/cjb-org-mode-config.el
@@ -3,5 +3,7 @@
 (setq org-todo-keywords
       '((sequence "TODO" "WORKING" "|" "DONE")))
 (setq org-todo-keyword-faces '(("WORKING" . (:foreground "yellow" :background "black"))))
+(setq org-startup-indented t)
+(add-hook 'org-mode-hook 'visual-line-mode)
 
 (provide 'cjb-org-mode-config)
