61a5ec765cec42ee81a150b90ea40756ca05a7a8
Casey Brant <casey@caseybrant.com>
2020-02-07 17:02:56 -0600

    Add Markdown to the org export list

--- emacs/cjb-org-mode-config.el
+++ emacs/cjb-org-mode-config.el
@@ -4,6 +4,7 @@
       '((sequence "TODO" "WORKING" "|" "DONE")))
 (setq org-todo-keyword-faces '(("WORKING" . (:foreground "yellow" :background "black"))))
 (setq org-startup-indented t)
+(setq org-export-backends (quote (ascii html latex md)))
 (add-hook 'org-mode-hook 'visual-line-mode)
 
 (provide 'cjb-org-mode-config)
