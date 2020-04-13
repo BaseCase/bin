6ea4d697085001dad28fddd1e0a560d9ccdf2569
Casey Brant <casey@caseybrant.com>
2020-04-13 10:56:54 -0500

    mac option Super in macOS (experimental); also fix default bindings for cell move

--- emacs/cjb-org-mode-config.el
+++ emacs/cjb-org-mode-config.el
@@ -7,4 +7,11 @@
 (setq org-export-backends (quote (ascii html latex md)))
 (add-hook 'org-mode-hook 'visual-line-mode)
 
+;; Keybindings really should go in the keybindings file, but these are described in the manual as defaults,
+;; so I dunno why they don't just work :shrug:
+(define-key evil-normal-state-map (kbd "s-<up>")    'org-table-move-cell-up)
+(define-key evil-normal-state-map (kbd "s-<down>")  'org-table-move-cell-down)
+(define-key evil-normal-state-map (kbd "s-<left>")  'org-table-move-cell-left)
+(define-key evil-normal-state-map (kbd "s-<right>") 'org-table-move-cell-right)
+
 (provide 'cjb-org-mode-config)
